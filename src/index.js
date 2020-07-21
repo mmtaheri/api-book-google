0a1a9b8b4d316941177baf4aef452ed4652561b1
mohaamad taheri <mmtaherie@gmail.com>
2020-07-21 17:19:56 +0430

    edit css-js add search

--- src/index.js
+++ src/index.js
@@ -114,24 +114,38 @@
   }
 
 
-  //search('[]')
+  search('[]')
   //////
-  let inputs = document.querySelector('.search')
+  let inputs = document.querySelector('.search');
+  let btnSearch = document.querySelector('.btn-search');
+  
   console.log(inputs)
   //let vals = inputs.value;
   //console.log(vals)
-  inputs.addEventListener('input', evt => {
-      const value = inputs.value.trim()
-      if(value){
-         search(value);
-          //console.log('fill')
+//   inputs.addEventListener('input', evt => {
+//       const value = inputs.value.trim()
+//       if(value){
+//          search(value);
+//           //console.log('fill')
      
-      }else {
-        //console.log('unfill');
-        search('[]')
-      }
+//       }else {
+//         //console.log('unfill');
+//         search('[]')
+//       }
    
-  })
+//   })
+btnSearch.onclick('input', evt => {
+    const value = inputs.value.trim()
+    if(value){
+       search(value);
+        //console.log('fill')
+   
+    }else {
+      //console.log('unfill');
+      search('[]')
+    }
+ 
+})
 
 /////WAY  3////
 // async function getBooks() {
