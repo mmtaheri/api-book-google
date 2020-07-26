ed3a2f5024393e09cebdc51c8d2624fc00681767
mohaamad taheri <mmtaherie@gmail.com>
2020-07-26 22:07:03 +0430

    add cashe & export style file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
 module.exports = {
+ 
+ 
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        // loaders: [
-        //     // {
-        //     //     test: /\.js$/,
-        //     //     loader: 'babel-loader',
-        //     //     exclude: /node_modules/
-        //     // }
-        // ],
+   
         rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader,'css-loader'],
+          },
+            // {
+            //     test: /\.css$/,
+            //     use: ['style-loader', 'css-loader']
+            // },
             {
                 test: /\.m?js$/,
                 exclude: /(node_modules)/,
@@ -33,10 +36,17 @@ module.exports = {
                 }
               },
             {
-                        test: /\.s[ac]ss$/,
-                        use: ['style-loader', 'css-loader','sass-loader']
+                 test: /\.s[ac]ss$/,
+                 use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
             }
         ]
     },
+    plugins: [
+     
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[id].[contenthash].css',
+      }),
+    ],
 
 };
